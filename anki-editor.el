anki-editor/anki-editor

commit be2fed0b47c992c9fe5ccb8cf9a72df9e47cdcd3

--note-contents-before-subheading: Check for last note in buffer
The old code failed to upload things like

    ** Note name
    Back text

when it was the last note in the buffer, since the loop could not find a
new heading and never terminated. As such, explicitly check for this
case.

Fixes: https://github.com/anki-editor/anki-editor/issues/38

anki-editor.el
@@ -900,10 +900,14 @@ Leading whitespace, drawers, and planning content is skipped."
 			 for nextelem = (progn
 					  (goto-char eoh)
 					  (org-element-at-point))
-			 while (not (memq (org-element-type nextelem)
-					  '(headline)))
-			 finally return (and eoh (org-element-property
-						  :begin nextelem))))
+			 while (not (or (memq (org-element-type nextelem)
+					    '(headline))
+                                      (eobp)))
+			 finally
+                         return (let ((end (org-element-property :begin nextelem)))
+                                  (and eoh (if (equal end begin)
+                                               (org-element-property :end nextelem)
+                                             end)))))
 	   (contents-raw (or (and begin
 				  end
 				  (buffer-substring-no-properties

commit c2d66f81382d0c0eb948f67c2cb8152e2b800f59

--note-contents-before-subheading: More carefully check for eobp
We don't need to keep track of the last note if we just check for eobp
before returning the point again.

Fixes: be2fed0b47c992c9fe5ccb8cf9a72df9e47cdcd3

anki-editor.el
@@ -901,13 +901,11 @@ Leading whitespace, drawers, and planning content is skipped."
                                           (goto-char eoh)
                                           (org-element-at-point))
                          while (not (or (memq (org-element-type nextelem)
-                                              '(headline))
-                                        (eobp)))
-                         finally
-                         return (let ((end (org-element-property :begin nextelem)))
-                                  (and eoh (if (equal end begin)
-                                               (org-element-property :end nextelem)
-                                             end)))))
+                                            '(headline))
+                                      (eobp)))
+                         finally return (and eoh (if (eobp)
+                                                     (org-element-property :end nextelem)
+                                                   (org-element-property :begin nextelem)))))
            (contents-raw (or (and begin
                                   end
                                   (buffer-substring-no-properties