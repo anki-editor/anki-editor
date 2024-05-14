60112a48ecf6b2f866f2a5a62be38247509f81a0
Renat Galimov <renat2017@gmail.com>
2024-05-14 22:14:48 +0300

    Added unit-tests for example.org file

--- anki-editor-tests.el
+++ anki-editor-tests.el
@@ -97,6 +97,7 @@ You can restore the original values by calling
   "Setup testing."
   (anki-editor-test--patch-variables
    '((anki-editor-api-port . 28765)
+     (anki-editor-prepend-heading . nil)
      (org-attach-dir-relative . t)
      (anki-editor-prepend-heading-format . "test *%s*")
      (org-html-link-use-abs-url . nil)))
@@ -139,6 +140,7 @@ Front content
 
 "))))
 
+
 (defun anki-editor-test--go-to-headline (title)
   "Go to headline with TITLE."
   (goto-char (point-min))
@@ -146,12 +148,13 @@ Front content
 
 (defun anki-editor-test--test-org-buffer (name)
   "Return test org buffer with NAME."
-  (find-file-noselect (expand-file-name name (expand-file-name "test-files" (file-name-directory (symbol-file 'anki-editor-test--go-to-headline))))))
+  (find-file-noselect (expand-file-name name (file-name-directory (symbol-file 'anki-editor-test--go-to-headline)))))
+
 
 (ert-deftest test--note-at-point-should-return-note-at-point ()
   "Test `anki-editor--note-at-point' should return note at point."
   (save-window-excursion
-    (with-current-buffer (anki-editor-test--test-org-buffer "test.org")
+    (with-current-buffer (anki-editor-test--test-org-buffer "test-files/test.org")
       (anki-editor-test--go-to-headline "Simple note")
       (anki-editor-test--setup)
       (unwind-protect
@@ -172,7 +175,7 @@ Simple note body
 (ert-deftest test--note-at-point-for-note-with-property-field-should-render-property-field ()
   "Test `anki-editor--note-at-point' should render property field."
   (save-window-excursion
-    (with-current-buffer (anki-editor-test--test-org-buffer "property-fields.org")
+    (with-current-buffer (anki-editor-test--test-org-buffer "test-files/property-fields.org")
       (anki-editor-test--go-to-headline "\"Front\" property field")
       (anki-editor-test--setup)
       (unwind-protect
@@ -187,7 +190,7 @@ Simple note body
 (ert-deftest test--note-at-point-for-note-with-property-field-should-override-subheading-field ()
   "Test `anki-editor--note-at-point' should override subheading field."
   (save-window-excursion
-    (with-current-buffer (anki-editor-test--test-org-buffer "property-fields.org")
+    (with-current-buffer (anki-editor-test--test-org-buffer "test-files/property-fields.org")
       (anki-editor-test--go-to-headline "\"Front\" property field with \"Front\" subheading")
       (anki-editor-test--setup)
       (unwind-protect
@@ -203,7 +206,7 @@ Simple note body
 (ert-deftest test--note-at-point-for-note-with-unknown-property-field-should-raise-error ()
   "Test `anki-editor--note-at-point' should raise error for unknown property field."
   (save-window-excursion
-    (with-current-buffer (anki-editor-test--test-org-buffer "property-fields.org")
+    (with-current-buffer (anki-editor-test--test-org-buffer "test-files/property-fields.org")
       (anki-editor-test--go-to-headline "Foreign property field")
       (anki-editor-test--setup)
       (unwind-protect
@@ -212,6 +215,73 @@ Simple note body
             (should-error (anki-editor-note-at-point) :type 'user-error)
             ' (user-error "Failed to map all named fields")))
         (anki-editor-test--teardown)))))
+
+(ert-deftest test--note-at-point-for-examples-should-produce-correct-output ()
+  "Test `anki-editor--note-at-point' should produce correct output for examples."
+  (let ((test-items-alist
+         '(("Deck in file" . #s(anki-editor-note
+                                nil "Cloze" "Default"
+                                (("Back Extra" . "<p>\nDeck in file</p>\n")
+                                 ("Text" . "<p>\nCards of this note will be created in {{c1::Default::which deck?}}\n</p>\n"))
+                                nil))
+           ("Deck in entry" . #s(anki-editor-note
+                                 nil "Cloze" "Languages"
+                                 (("Back Extra" . "<p>\nDeck in entry</p>\n")
+                                  ("Text" . "<p>\nCards of this note will be created in {{c1::Languages::which deck?}}\n</p>\n"))
+                                 nil))
+           ("Raw fields" . #s(anki-editor-note
+                              nil "Basic" "Default"
+                              (("Back" . "<p>\nWith property &lt;code&gt;:ANKI<sub>FORMAT</sub>: nil&lt;/code&gt;, content of the\nfield will be sent to Anki &lt;em&gt;unprocessed&lt;/em&gt;.  You can use\nwhatever Anki supports, like HTML tags.\n&lt;br&gt;\n&lt;br&gt;\nThis property is retrieved with inheritance, meaning that it can be\nset in any ancestor entries or at the top of the file with\n&lt;code&gt;#+PROPERTY: ANKI<sub>FORMAT</sub> nil&lt;/code&gt;, it's also possible to\noverride an outer level nil format with &lt;code&gt;:ANKI<sub>FORMAT</sub>: t&lt;/code&gt;.\n</p>\n")
+                               ("Front" . "<p>\nHow to send the content of a field or fields to Anki as is?\n</p>\n"))
+                              nil))
+           ("Is there a shorter way to write notes?" . #s(anki-editor-note
+                                                          nil "Basic" "Default"
+                                                          (("Back" . "<p>\nYes, like this one, Front is missing, <code>anki-editor</code> will use note\nheading as Front.  This is neat as sometimes it's verbose to repeat\nthe same content in note heading and first field.\n</p>\n\n<p>\nThis works for all note types, just make one field absent and\n<code>anki-editor</code> will use note heading as that missing field.\n</p>\n")
+                                                           ("Front" . "<p>\nIs there a shorter way to write notes?</p>\n"))
+                                                          nil))
+           ("Raining" . #s(anki-editor-note
+                           nil "Basic (and reversed card)" "Languages"
+                           (("Back" . "<p>\nit's raining very hard\n</p>\n")
+                            ("Front" . "<p>\n(it's) raining cats and dogs\n</p>\n"))
+                           ("vocab" "idioms" "english")))
+           ("名词从句" . #s(anki-editor-note
+                            nil "Basic" "Languages"
+                            (("Back" . "<ol class=\"org-ol\">\n<li>That + 一个完整的句子, that无实际意义</li>\n<li>由疑问句改装而成</li>\n</ol>\n")
+                             ("Front" . "<p>\n名词从句有哪些形式？\n</p>\n"))
+                            ("grammar" "english")))
+           ("Cantonese" . #s(anki-editor-note
+                             nil "Basic (and reversed card)" "Languages"
+                             (("Back" . "<p>\n吃过饭了没？\n</p>\n")
+                              ("Front" . "<p>\n食咗饭未吖？\n</p>\n"))
+                             ("cantonese" "dialect")))
+           ("Emacs Lisp" . #s(anki-editor-note
+                              nil "Basic" "Computing"
+                              (("Back" . "<div align=\"left\">\n\n<div class=\"org-src-container\">\n<pre class=\"src src-emacs-lisp\">(condition-case the-error\n    ;; the protected form\n    (progn\n      (do-something-dangerous)\n      (do-something-more-dangerous))\n  ;; handlers\n  (error-symbol1 (handler1 the-error))\n  ((error-symbol2 error-symbol3 (handler the-error))))\n</pre>\n</div>\n\n</div>\n")
+                               ("Front" . "<p>\nHow to trap errors in emacs lisp?\n</p>\n"))
+                              ("lisp" "emacs" "programming")))
+           ("Dot product" . #s(anki-editor-note
+                               nil "Basic" "Mathematics"
+                               (("Back" . "<p>\n<p>[$$]\\alpha \\cdot \\beta = a_1b_1 + a_2b_2 + a_3b_3[/$$]</p>\n</p>\n")
+                                ("Front" . "<p>\nHow to calculate the dot product of two vectors:\n</p>\n\n[latex]<br>\\begin{equation*}<br>\\alpha = \\{a_1, a_2, a_3\\}, \\beta = \\{b_1, b_2, b_3\\}<br>\\end{equation*}<br>[/latex]\n"))
+                               nil))
+           )))
+    (save-window-excursion
+      (with-current-buffer (anki-editor-test--test-org-buffer "examples.org")
+        (anki-editor-test--setup)
+        (org-map-entries
+         (lambda ()
+           ;; Only entries which have ANKI_NOTE_TYPE
+           (when (org-entry-get (point) "ANKI_NOTE_TYPE")
+             (unwind-protect
+                 (let ((note-at-point nil)
+                       (headline nil)
+                       (expected-note nil))
+                   (setq headline (org-entry-get (point) "ITEM"))
+                   (setq note-at-point (anki-editor-note-at-point))
+                   (setq expected-note (cdr (assoc headline test-items-alist)))
+                   (should (equal note-at-point expected-note)))
+               ))))))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; anki-editor-tests.el ends here
