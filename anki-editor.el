anki-editor/anki-editor

commit 6c2b1208955b347ebe504345cfc4cca09915f69e

Always add paragraphs for display maths
This is in line with how LaTeX itself treats display maths. It allows
one to write

    abc
    \[
       1
    \]
    def

instead of having to write

    abc

    \[
       1
    \]

    def

anki-editor.el
@@ -241,7 +241,7 @@ Raise an error if applicable."
 
 (defmacro anki-editor-api-with-multi (&rest body)
   "Use in combination with `anki-editor-api-enqueue' to combine
-multiple api calls into a single 'multi' call, return the results
+multiple api calls into a single `multi' call, return the results
 of these calls in the same order."
   `(let (--anki-editor-var-multi-actions--
          --anki-editor-var-multi-results--)
@@ -352,23 +352,25 @@ The result is the path to the newly stored media file."
            for matches = (string-match (cl-first delims) latex-code)
            when matches
            do
-           (setq latex-code (replace-match
-                             (concat (if (and anki-editor-create-latex-display-math-div
-                                              (--any? (s-matches? (cl-first delims) it)
-                                                      '("\\[" "$$")))
-                                         "<div class=\"display-math\">"
-                                       "")
-                                     (cl-second delims))
-                             t t latex-code))
-           (string-match (cl-third delims) latex-code)
-           (setq latex-code (replace-match
-                             (concat (cl-fourth delims)
-                                     (if (and anki-editor-create-latex-display-math-div
-                                              (--any? (s-matches? (cl-third delims) it)
-                                                      '("\\]" "$$")))
-                                         "</div>"
-                                       ""))
-                             t t latex-code))
+           (let ((display-beg (--any? (s-matches? (concat (cl-first delims) "$") it) '("\\[" "$$")))
+                 (display-end (--any? (s-matches? (cl-third delims)              it) '("\\]" "$$"))))
+             (setq latex-code (replace-match
+                               (concat (if display-beg "<p>" "")
+                                       (if (and anki-editor-create-latex-display-math-div
+                                                display-beg)
+                                           "<div class=\"display-math\">"
+                                         "")
+                                       (cl-second delims))
+                               t t latex-code))
+             (string-match (cl-third delims) latex-code)
+             (setq latex-code (replace-match
+                               (concat (cl-fourth delims)
+                                       (if (and anki-editor-create-latex-display-math-div
+                                                display-end)
+                                           "</div>"
+                                         "")
+                                       (if display-end "</p>" ""))
+                               t t latex-code)))
            until matches
            finally return latex-code))
 

commit cac439cf6176a383a07210f56dfe65147c4ec8cf

Correctly match display maths end when exporting
Fixes: https://github.com/anki-editor/anki-editor/issues/56
Fixes: 6c2b1208955b347ebe504345cfc4cca09915f69e

anki-editor.el
@@ -372,7 +372,7 @@ The result is the path to the newly stored media file."
              when matches
              do
              (let ((display-beg (matches? (concat (cl-first delims) "$") '("\\[" "$$")))
-                   (display-end (matches? (cl-third delims)              '("\\]" "$$"))))
+                   (display-end (matches? (concat "^" (cl-third delims)) '("\\]" "$$"))))
                (setq latex-code (replace-match
                                  (concat (if display-beg
                                              (concat "<p>" (anki-editor--latex-div-beg))